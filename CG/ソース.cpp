9daa8b2e5fa39cff429e7711398b396692a435b8
igu-ryo <49966687+igu-ryo@users.noreply.github.com>
2020-06-28 12:38:27 +0900

    課題1初期

--- CG/ソース.cpp
+++ CG/ソース.cpp
@@ -1,10 +1,10 @@
 #define FREEGLUT_STATIC
-#define NOMINMAX	// Windows 上で min/max のマクロを無効化するためのおまじない 
+#define NOMINMAX
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <GL/glut.h>
 #include <math.h>   
-#include <algorithm> // 小さい方の値を返す std::min 関数を使うため
+#include <algorithm>
 
 class Vector3d {
 public:
@@ -103,25 +103,94 @@ public:
 	}
 };
 
+// 板。xz平面に平行な面とする
+class Board {
+public:
+	double y; // y座標値
+
+	Board(double _y) {
+		y = _y;
+	}
+
+	// 点pを通り、v方向のRayとの交わりを判定する。
+	// 交点が p+tv として表せる場合の t の値を返す。交わらない場合は負の値を返す
+	double getIntersec(Vector3d& p, Vector3d& v) {
+		if (fabs(v.y) < 1.0e-10) return -1; // 水平なRayは交わらない
+
+		double t = -1;
+		// ★ここで t の値を計算する
+		// ★ただしz座標が-3000より小さいなら交わらないものとする
+
+		return t;
+	}
+
+	// x と z の値から床の色を返す（格子模様になるように）
+	Vector3d getColorVec(double x, double z) {
+		// ★ x, z の値によって(1.0, 1.0, 0.7)または(0.6, 0.6, 0.6)のどちらかの色を返すようにする
+
+		return Vector3d(0.6, 0.6, 0.6);
+	}
+};
 
 int halfWidth;    // 描画領域の横幅/2
 int halfHeight;   // 描画領域の縦幅/2
-
-// 各種定数
-double d = 1000;  // 視点と投影面との距離
+double screen_z = -1000;  // 投影面のz座標
 double Kd = 0.8;  // 拡散反射定数
 double Ks = 0.8;  // 鏡面反射定数
-double Iin = 1.0; // 入射光の強さ 
-double Ia = 0.2; // 環境光
-
-Vector3d viewPosition(0, 0, 0); // 視点位置
+double Iin = 0.5; // 入射光の強さ 
+double Ia = 0.5; // 環境光
+Vector3d viewPosition(0, 0, 0); // 原点=視点
 Vector3d lightDirection(-2, -4, -2); // 入射光の進行方向
+
 
 // レンダリングする球体
 Sphere sphere(0.0, 0.0, -1500, // 中心座標
 	150.0,           // 半径
-	0.2, 0.9, 0.9);  // RGB値
+	0.1, 0.7, 0.7);  // RGB値
 
+// 球体の置かれている床
+Board board(-150); // y座標値を -150 にする。（球と接するようにする）
+
+// 2つのベクトルの成す角のcos値を計算する
+double getCos(Vector3d& v0, Vector3d& v1) {
+	return (v0.x * v1.x + v0.y * v1.y + v0.z * v1.z) / (v0.length() * v1.length());
+}
+
+// x, y で指定されたスクリーン座標での色(RGB)を colorVec のxyzの値に格納する
+void getPixelColor(double x, double y, Vector3d& colorVec) {
+	// 原点からスクリーン上のピクセルへ飛ばすレイの方向
+	Vector3d ray(x - viewPosition.x, y - viewPosition.y, screen_z - viewPosition.z);
+
+	ray.normalize(); // レイの長さの正規化
+
+	// レイを飛ばして球と交差するか求める
+	double t_sphere = sphere.getIntersec(viewPosition, ray);
+
+	if (t_sphere > 0) { // 球との交点がある
+		// ★前回の課題を参考に、球体の表面の色を計算で求め、colorVecに設定する
+		double r = 0;
+		double g = 0;
+		double b = 0;
+		colorVec.set(r, g, b);
+		return;
+	}
+
+	// レイを飛ばして床と交差するか求める
+	double t_board = board.getIntersec(viewPosition, ray);
+
+	if (t_board > 0) { // 床との交点がある
+		// ★床の表面の色を設定する
+		// ★球の影になる場合は、RGBの値をそれぞれ0.5倍する
+		double r = 0;
+		double g = 0;
+		double b = 0;
+		colorVec.set(r, g, b);
+		return;
+	}
+
+	// 何とも交差しない
+	colorVec.set(0, 0, 0); // 背景色（黒）を設定する
+}
 
 // 描画を行う
 void display(void) {
@@ -132,41 +201,13 @@ void display(void) {
 	for (int y = (-halfHeight); y <= halfHeight; y++) {
 		for (int x = (-halfWidth); x <= halfWidth; x++) {
 
-			Vector3d ray(x - viewPosition.x, y - viewPosition.y, -d - viewPosition.z); // 原点からスクリーン上のピクセルへ飛ばすレイの方向
-			ray.normalize(); // レイの長さの正規化
+			Vector3d colorVec;
 
-			// レイを飛ばして球との交点を求める
-			double t = sphere.getIntersec(viewPosition, ray);
+			// x, y 座標の色を取得する
+			getPixelColor(x, y, colorVec);
 
-			if (t > 0) { // 交点がある
-				double Is = 0; // 鏡面反射光
-				double Id = 0; // 拡散反射光
-
-				// ★ここで Is および Id の値を計算する
-				Vector3d N = (viewPosition + t * ray) - sphere.center; // (交点)-(円の中心)=(法線ベクトル)
-				N.normalize();
-				double cosAlpha = (-lightDirection * N);
-				Id = cosAlpha > 0 ? Iin * Kd * cosAlpha : 0;
-
-				double a = cosAlpha; // a=-L*NであるのでcosAlphaの値を利用
-				Vector3d R = lightDirection + 2 * a * N; // 反射方向
-				double cosGamma = -ray * R; // (視点方向)*(反射方向)
-				Is = cosAlpha > 0 && cosGamma > 0 ? Iin * Ks * pow(cosGamma, 5) : 0; // 「光源からの光が球に届いていて、かつ鏡面反射光が視点に届くとき」以外は0
-
-				double I = Id + Is + Ia;
-				double r = std::min(I * sphere.cR, 1.0); // 1.0 を超えないようにする
-				double g = std::min(I * sphere.cG, 1.0); // 1.0 を超えないようにする
-				double b = std::min(I * sphere.cB, 1.0); // 1.0 を超えないようにする
-
-				// 描画色の設定
-				glColor3d(r, g, b);
-
-			}
-			else { // 交点が無い
-
-			 // 描画色を黒にする
-				glColor3f(0.0f, 0.0f, 0.0f);
-			}
+			//取得した色で、描画色を設定する
+			glColor3d(colorVec.x, colorVec.y, colorVec.z);
 
 			// (x, y) の画素を描画
 			glBegin(GL_POINTS);
@@ -177,8 +218,8 @@ void display(void) {
 	glFlush();
 }
 
-void resize(int w, int h) {
-	if (h < 1) return;
+void resizeWindow(int w, int h) {
+	h = (h == 0) ? 1 : h;
 	glViewport(0, 0, w, h);
 	halfWidth = w / 2;
 	halfHeight = h / 2;
@@ -203,14 +244,16 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(180, 10);
 	glutCreateWindow(argv[0]);
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glShadeModel(GL_FLAT);
 
 	glutDisplayFunc(display);
-	glutReshapeFunc(resize);
+	glutReshapeFunc(resizeWindow);
 	glutKeyboardFunc(keyboard);
 	glutMainLoop();
 
 	return 0;
 }
+
